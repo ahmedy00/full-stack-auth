3080e363353e9a31fa60c7b6e61c2e163d98997a
ahmet <dede_dede_ahmet@hotmail.com>
2024-05-25 10:01:45 +0300

    Axios interceptors added: Bearer token will be added to requests.

--- frontend/plugins/axios.ts
+++ frontend/plugins/axios.ts
@@ -7,6 +7,27 @@ export default defineNuxtPlugin(() => {
         baseURL: defaultUrl,
     })
 
+    api.interceptors.request.use(config => {
+        const token = localStorage.getItem('token')
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    }, error => {
+        return Promise.reject(error)
+    })
+
+    api.interceptors.response.use(response => {
+        return response
+    }, error => {
+        if (error.response.status === 401) {
+            // Token expired or invalid
+            localStorage.removeItem('token')
+            navigateTo('/auth/login')
+        }
+        return Promise.reject(error)
+    })
+
     return {
         provide: {
             axios: api,
