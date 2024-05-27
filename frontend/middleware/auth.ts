b871ca533309cf36a9091989f15366f3dd258c48
ahmet <dede_dede_ahmet@hotmail.com>
2024-05-28 00:34:00 +0300

    Changed redirect logic: Users that not logged in will not redirect to login page from singup page.

--- frontend/middleware/auth.ts
+++ frontend/middleware/auth.ts
@@ -2,10 +2,11 @@ export default defineNuxtRouteMiddleware(to => {
     const { setSnackbar } = useSnackbarStore()
     if (process.client) {
         const token = localStorage.getItem('token')
-        if (!token && to.path !== '/auth/login') {
+        if (!token && (to.path !== '/auth/login' && to.path !== '/auth/signup')) {
             setSnackbar(true, '', 'You need to login first!')
             return navigateTo('/auth/login')
-        } else if (token && to.path === '/auth/login') {
+        } else if (token && (to.path === '/auth/login' || to.path === '/auth/signup')) {
+            setSnackbar(true, '', 'You already logged in!')
             return navigateTo('/')
         }
     }
